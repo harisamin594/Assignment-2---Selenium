2a06ad7060909b197a09e7775a5ca4368e9102bc
Haris Amin <110045095+harisamin594@users.noreply.github.com>
2022-08-18 20:33:17 +0500

    Add files via upload

---
+++ 231494566_Assignment 2_ Selenium/src/seleniumdemo/TestCase_231494566.java
@@ -0,0 +1,32 @@
+package seleniumdemo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestCase {
+
+	public static void main(String[] args) {
+		System.setProperty("Webdriver.chrome.driver", "C:\\browserdriver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.gmail.com/");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("identifierId")).sendKeys("harisamin594@gmail.com");
+		driver.findElement(By.className("VfPpkd-RLmnJb")).click();
+		String at = driver.getTitle();
+		String et = "gmail";
+		
+		driver.close();
+
+		if(at.equalsIgnoreCase(et))
+		{
+			System.out.println("Test Successful!");
+		}
+		else
+		{
+			System.out.println("Test Failed!");
+		}
+
+	}
+
+}
